5f3edd85f557b8e99c022b85d32b5b57c8d00e17
Muhamad Anan Makrifsyah Gani <40011655+ananharunoanzu@users.noreply.github.com>
2018-06-07 11:37:02 +0800

    Final_E1E115031_MUHAMAD_ANAN_MAKRIFSYAH_GANI

--- bola.py
+++ bola.py
@@ -6,13 +6,19 @@ tinggi = 400
 
 tk = Tk()
 canvas = Canvas(tk, width=lebar, height=tinggi)
-tk.title("Gambar Bola Bergerak")
+tk.title("Gambar Bola Banyak Bergerak")
 
 canvas.pack()
 
-bola = canvas.create_oval(10, 10, 70, 70, fill="orange")
+bola = canvas.create_oval(10, 10, 70, 70, fill="yellow")
+bola2 = canvas.create_oval(10, 10, 70, 70, fill="red")
+bola3 = canvas.create_oval(10, 10, 70, 70, fill="blue")
 kecepatanx = 4
-kecepatany = 5
+kecepatany = 1
+kecepatanx = 3
+kecepatany = 2
+kecepatanx = 3
+kecepatany = 1
 
 while True:
     canvas.move(bola, kecepatanx, kecepatany)
@@ -23,6 +29,23 @@ while True:
         kecepatanx = -kecepatanx
     tk.update()
     time.sleep(0.001)
-print("Coba")
+
+    canvas.move(bola2, kecepatanx, kecepatany)
+    pos = canvas.coords(bola2)
+    if pos[3] >= tinggi or pos[1] <= 0:
+        kecepatany = -kecepatany
+    if pos[2] >= lebar or pos[0] <= 0:
+        kecepatanx = -kecepatanx
+    tk.update()
+    time.sleep(0.001)
+
+    canvas.move(bola3, kecepatanx, kecepatany)
+    pos = canvas.coords(bola3)
+    if pos[3] >= tinggi or pos[1] <= 0:
+        kecepatany = -kecepatany
+    if pos[2] >= lebar or pos[0] <= 0:
+        kecepatanx = -kecepatanx
+    tk.update()
+    time.sleep(0.001)
 
 tk.mainloop()
